aa6997aa2fd9e1855995b1d119eed1162d728e5a
Tan Nguyen <tannguyen5659@gmail.com>
2024-03-30 21:47:55 -0700

    first

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_smorest import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+from Config import Config
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+api = Api(app)
+jwt = JWTManager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models.user_model import UserModel
+from models.post_model import PostModel
+from models.like_post_model import LikePostModel
+
+from resources.post import bp as post_bp
+app.register_blueprint(post_bp)
+from resources.user import bp as user_bp
+app.register_blueprint(user_bp)
+from resources.like_post import bp as like_post_bp
+app.register_blueprint(like_post_bp)
+
